fa94db546eee4783e62a1069ac1920f653aa86de
Jackkeetch <eugene.polcanov@gmail.com>
2025-07-03 17:39:19 +0300

    finish

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,17 @@
-function getElementWidth(content, padding, border) {
-  const contentValue = Number.parseFloat(content);
-  const contentPadding = Number.parseFloat(padding);
-  const contentBorder = Number.parseFloat(border);
-  return contentValue + contentPadding * 2 + contentBorder * 2;
+function checkForSpam(message) {
+  // let inputNormalize = message.toLowerCase() ? return true : inputNormalize.includes("sale", "spam");
+  let inputNormalize = message.toLowerCase();
+  if (inputNormalize.includes("sale") || inputNormalize.includes("spam")) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
